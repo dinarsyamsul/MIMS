db5d784c6738b5230884833692b33a4f03445dd1
dinarsyamsul <dinarsyamsul@gmail.com>
2022-10-26 00:00:11 +0700

    +++

---
+++ app/src/main/java/dev/iconpln/mims/ui/DashboardPusertifActivity.kt
@@ -0,0 +1,11 @@
+package dev.iconpln.mims.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardPusertifActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_pusertif)
+    }
+}
