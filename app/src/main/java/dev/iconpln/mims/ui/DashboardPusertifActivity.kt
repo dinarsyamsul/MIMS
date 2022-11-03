8408a41e5dfa27b771b45f09634a239db54d731e
ilhaam1552 <ilham.ardiansyah@iconpln.co.id>
2022-11-03 14:17:55 +0700

    tambah ui monitoring purchase order, upload data material

--- app/src/main/java/dev/iconpln/mims/ui/DashboardPusertifActivity.kt
+++ app/src/main/java/dev/iconpln/mims/ui/DashboardPusertifActivity.kt
@@ -2,10 +2,13 @@ package dev.iconpln.mims.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.iconpln.mims.R
 
 class DashboardPusertifActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_pusertif)
+
+        //cobian run keunyoay
     }
 }
