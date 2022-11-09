323a5831aeff201d7079148f181676f22fc7566a
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2022-11-09 09:54:38 +0700

    fixing login ui

--- app/src/main/java/dev/iconpln/mims/utils/Constants.kt
+++ app/src/main/java/dev/iconpln/mims/utils/Constants.kt
@@ -1,7 +1,7 @@
 package dev.iconpln.mims.utils
 
 object Constants {
-    const val AGO_BASE_URL = "http://10.14.78.58:8080/"
+    const val AGO_BASE_URL = "http://10.14.53.64:8080/"
     const val BASE_URL = "http://10.14.78.29:8000/"
 
 }
