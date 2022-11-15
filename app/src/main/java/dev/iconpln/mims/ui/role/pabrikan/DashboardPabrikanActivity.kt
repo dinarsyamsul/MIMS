7036c1ac72b55ad13b465dc76ac0c502bf7fa37d
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2022-11-15 10:51:45 +0700

    fixing dashboard pabrikan UI

---
+++ app/src/main/java/dev/iconpln/mims/ui/role/pabrikan/DashboardPabrikanActivity.kt
@@ -0,0 +1,33 @@
+package dev.iconpln.mims.ui.role.pabrikan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dev.iconpln.mims.R
+import dev.iconpln.mims.databinding.ActivityDashboardPabrikanBinding
+
+class DashboardPabrikanActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardPabrikanBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardPabrikanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navViewPabrikan
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
--- app/src/main/java/dev/iconpln/mims/ui/role/pusertif/DashboardPusertifActivity.kt
+++ app/src/main/java/dev/iconpln/mims/ui/role/pusertif/DashboardPusertifActivity.kt
@@ -1,12 +1,14 @@
-package dev.iconpln.mims.ui
+package dev.iconpln.mims.ui.role.pusertif
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.iconpln.mims.R
 
-class DashboardPabrikanActivity : AppCompatActivity() {
+class DashboardPusertifActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_pabrikan)
+        setContentView(R.layout.activity_dashboard_pusertif)
+
+        //cobian run keunyoay
     }
 }
