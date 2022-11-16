a4e1d3997992bf2a1014d7c4d0281acb82923b59
ilhaam1552 <ilham.ardiansyah@iconpln.co.id>
2022-11-16 11:25:32 +0700

    Fixing Dashboard UI

--- app/src/main/java/dev/iconpln/mims/Profile_Fragment.kt
+++ app/src/main/java/dev/iconpln/mims/Profile_Fragment.kt
@@ -1,59 +1,63 @@
 package dev.iconpln.mims
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import dev.iconpln.mims.databinding.FragmentProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Profile_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Profile_Fragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var muncul = false
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Profile_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Profile_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        binding.img1.setOnClickListener{
+//            if (muncul == true){
+//                binding.img1.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_keyboard_arrow_up_24))
+//                binding.const2.visibility = View.VISIBLE
+//                muncul = !muncul
+//
+//            } else {
+//                binding.img1.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_keyboard_arrow_down_24))
+//                binding.const2.visibility = View.GONE
+//                muncul = !muncul
+//            }
+//        }
+//
+//        binding.btn1.setOnClickListener {
+//            val  dialogBinding = layoutInflater.inflate(R.layout.activity_popdialog, null)
+//
+//            val myDialog = Dialog(requireContext())
+//            myDialog.setContentView(dialogBinding)
+//
+//            myDialog.setCancelable(true)
+//            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//            myDialog.show()
+//        }
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        _binding = null
+//    }
+
 }
