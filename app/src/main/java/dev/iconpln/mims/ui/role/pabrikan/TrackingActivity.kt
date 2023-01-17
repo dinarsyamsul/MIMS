46fb723ef8b1b3acfdb2e293659818cecf2a55f2
dinarsyamsul <dinarsyamsul@gmail.com>
2023-01-17 18:53:36 +0700

    Add TrackingActivity.kt and layoutnya

---
+++ app/src/main/java/dev/iconpln/mims/ui/role/pabrikan/TrackingActivity.kt
@@ -0,0 +1,12 @@
+package dev.iconpln.mims.ui.role.pabrikan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.iconpln.mims.R
+
+class TrackingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tracking)
+    }
+}
