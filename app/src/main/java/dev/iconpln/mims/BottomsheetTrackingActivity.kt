65263f5d2606bc132ae45ed24a292353c447702e
ilhaam1552 <ilham.ardiansyah@iconpln.co.id>
2023-01-27 04:59:43 +0700

    ***

--- app/src/main/java/dev/iconpln/mims/BottomsheetTrackingActivity.kt
+++ app/src/main/java/dev/iconpln/mims/BottomsheetTrackingActivity.kt
@@ -8,16 +8,20 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dev.iconpln.mims.databinding.ActivityBottomsheetTrackingBinding
+import dev.iconpln.mims.databinding.FragmentHomeBinding
 
 class BottomsheetTrackingActivity : BottomSheetDialogFragment() {
-    
-    private lateinit var binding: ActivityBottomsheetTrackingBinding
+
+    private var _binding: ActivityBottomsheetTrackingBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.activity_bottomsheet_tracking, container, false)
+        _binding = ActivityBottomsheetTrackingBinding.inflate(inflater, container,false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/dev/iconpln/mims/ui/role/pabrikan/tracking/TrackingHistoryActivity.kt
+++ app/src/main/java/dev/iconpln/mims/ui/role/pabrikan/tracking/TrackingHistoryActivity.kt
@@ -1,23 +1,40 @@
 package dev.iconpln.mims.ui.role.pabrikan.tracking
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Toast
 import androidx.activity.viewModels
+import dev.iconpln.mims.BottomsheetTrackingActivity
 import dev.iconpln.mims.MyApplication
 import dev.iconpln.mims.R
 import dev.iconpln.mims.data.local.database.DaoSession
+import dev.iconpln.mims.databinding.ActivityTrackingBinding
+import dev.iconpln.mims.utils.PopupDialog
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class TrackingHistoryActivity : AppCompatActivity() {
     private val trackingViewModel: TrackingHistoryViewModel by viewModels()
+    private lateinit var binding: ActivityTrackingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tracking)
+        binding = ActivityTrackingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomsheetFragment = BottomsheetTrackingActivity()
+        val popupDialog = PopupDialog()
+
+
+        binding.btnScan.setOnClickListener {
+            bottomsheetFragment.show(supportFragmentManager,"silahkan scan bang")
+        }
 
         val sn = "PLN0219000022402314000000018"
         trackingViewModel.getTrackingHistory(sn, this)
@@ -26,6 +43,10 @@ class TrackingHistoryActivity : AppCompatActivity() {
             Toast.makeText(this, "${it.historis}", Toast.LENGTH_SHORT).show().also {
                 startActivity(Intent(this@TrackingHistoryActivity, DetailTrackingHistoryActivity::class.java))
             }
+        }
+
+        binding.btnInputManual.setOnClickListener {
+            popupDialog.show(supportFragmentManager,"Masukan SN Manual")
         }
     }
 }
