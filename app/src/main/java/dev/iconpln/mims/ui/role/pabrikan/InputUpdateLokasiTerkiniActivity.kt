ec9f87ef37cf9b401734cfb68961ff9f49eaf8eb
dinarsyamsul <dinarsyamsul@gmail.com>
2023-01-27 05:58:22 +0700

    baymax

---
+++ app/src/main/java/dev/iconpln/mims/ui/role/pabrikan/InputUpdateLokasiTerkiniActivity.kt
@@ -0,0 +1,11 @@
+package dev.iconpln.mims.ui.role.pabrikan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputUpdateLokasiTerkiniActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_update_lokasi_terkini)
+    }
+}
---
+++ app/src/main/java/dev/iconpln/mims/ui/role/pabrikan/LokasiTerkiniActivity.kt
@@ -0,0 +1,11 @@
+package dev.iconpln.mims.ui.role.pabrikan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LokasiTerkiniActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lokasi_terkini)
+    }
+}
