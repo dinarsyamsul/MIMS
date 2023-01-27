7d9074078a6565a3152fb0d32fdfbeb994218916
ilhaam1552 <ilham.ardiansyah@iconpln.co.id>
2023-01-27 07:02:01 +0700

    add ui tracking activity

---
+++ app/src/main/java/dev/iconpln/mims/ViewPenerimaanTrackingActivity.kt
@@ -0,0 +1,11 @@
+package dev.iconpln.mims
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPenerimaanTrackingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_penerimaan_tracking)
+    }
+}
