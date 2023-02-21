d9fc2c5b9e2042168f419c92d047dfb64e1ce612
dinarsyamsul <dinarsyamsul@gmail.com>
2023-02-22 05:25:21 +0700

    ++

---
+++ app/src/main/java/dev/iconpln/mims/ui/pnerimaan/InputSNMaterialPenerimaanPemeriksaanULPActivity.kt
@@ -0,0 +1,11 @@
+package dev.iconpln.mims.ui.pnerimaan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputSNMaterialPenerimaanPemeriksaanULPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_snmaterial_penerimaan_pemeriksaan_ulpactivity)
+    }
+}
