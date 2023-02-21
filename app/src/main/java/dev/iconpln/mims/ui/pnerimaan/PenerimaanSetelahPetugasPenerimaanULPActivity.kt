785cc412473054bec797aaab1a1ad7a1e393769e
dinarsyamsul <dinarsyamsul@gmail.com>
2023-02-21 15:41:43 +0700

    ++

--- app/src/main/java/dev/iconpln/mims/ui/pnerimaan/PenerimaanSetelahPetugasPenerimaanULPActivity.kt
+++ app/src/main/java/dev/iconpln/mims/ui/pnerimaan/PenerimaanSetelahPetugasPenerimaanULPActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.iconpln.mims.R
 
-class PenerimaanULPActivity : AppCompatActivity() {
+class PenerimaanSetelahPetugasPenerimaanULPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_penerimaan_ulpactivity)
+        setContentView(R.layout.activity_penerimaan_setelah_petugas_penerimaan_ulpactivity)
     }
 }
--- app/src/main/java/dev/iconpln/mims/ui/pnerimaan/PetugasPemeriksaPenerimaanULPActivity.kt
+++ app/src/main/java/dev/iconpln/mims/ui/pnerimaan/PetugasPemeriksaPenerimaanULPActivity.kt
@@ -2,11 +2,10 @@ package dev.iconpln.mims.ui.pnerimaan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import dev.iconpln.mims.R
 
-class PenerimaanULPActivity : AppCompatActivity() {
+class PetugasPemeriksaPenerimaanULPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_penerimaan_ulpactivity)
+        setContentView(R.layout.activity_petugas_pemeriksa_penerimaan_ulp)
     }
 }
