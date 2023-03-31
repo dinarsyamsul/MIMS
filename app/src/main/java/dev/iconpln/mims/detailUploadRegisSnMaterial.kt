445724effa1f99183265f8d890361149da6ae26a
ilhaam1552 <ilham.ardiansyah@iconpln.co.id>
2023-03-31 10:55:39 +0700

    adding penerimaan registrasi UI

---
+++ app/src/main/java/dev/iconpln/mims/detailUploadRegisSnMaterial.kt
@@ -0,0 +1,11 @@
+package dev.iconpln.mims
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detailUploadRegisSnMaterial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_upload_regis_sn_material)
+    }
+}
