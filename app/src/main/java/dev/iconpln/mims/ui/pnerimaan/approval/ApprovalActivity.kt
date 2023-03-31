0e56c497921dcf56a79f95d0a9486c574da0aed2
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2023-03-31 10:25:33 +0700

    Add UI Approval SN Material

---
+++ app/src/main/java/dev/iconpln/mims/ui/pnerimaan/approval/ApprovalActivity.kt
@@ -0,0 +1,13 @@
+package dev.iconpln.mims.ui.pnerimaan.approval
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.iconpln.mims.R
+
+class ApprovalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_approval)
+
+    }
+}
---
+++ app/src/main/java/dev/iconpln/mims/ui/pnerimaan/approval/DetailSNdanGenerateActivity.kt
@@ -0,0 +1,12 @@
+package dev.iconpln.mims.ui.pnerimaan.approval
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.iconpln.mims.R
+
+class DetailSNdanGenerateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_sndan_generate)
+    }
+}
